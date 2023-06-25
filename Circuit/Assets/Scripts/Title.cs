ff7366d8feba179342e239ec5b02bfd80fe105ca
OCensoredO <pgvdpb@gmail.com>
2023-06-25 22:01:07 +0900

    UI, 타이틀, 클리어 씬 추가

---
+++ Circuit/Assets/Scripts/Title.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) SceneManager.LoadScene("Stage1");
+    }
+}
